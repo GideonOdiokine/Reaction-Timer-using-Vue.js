c5943db80ae6c15625f5ae7392d9bcae9bb7bc23
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-02 01:15:18 +0100

    added rank to result components

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,10 +1,25 @@
 <template>
-  <h3>This is your scores: {{ score }}</h3>
+  <h3>This is your Time Reaction: {{ score }}</h3>
+  <p>Rank:: {{ rank }}</p>
 </template>
 
 <script>
 export default {
   props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Gidi Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = " Snail pace";
+    }
+  },
 };
 </script>
 
